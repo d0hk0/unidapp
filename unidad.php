d7a97ced5067288a7c3c1455bbd22cce7d01df45
d0hk0 <darkurt@gmail.com>
2015-09-09 22:41:56 -0500

    creacion y consulta en la fuente unidad.php
    falta acomodarla en algo mas limpio

--- unidad.php
+++ unidad.php
@@ -1,30 +1,52 @@
 <html>
 <head>
-	<title>Problema</title>
+	<title>Creacion de la unidad</title>
 </head>
 <body>
 	<?php
+	//creo la conexion
 	$db = new mysqli('localhost', 'root', '', 'bdunidapp');
 
+	//me conecto
 	if($db->connect_errno > 0){
 	    die('Unable to connect to database [' . $db->connect_error . ']');
 	}
 	
-	
-	$sql = "SQL insert into tbl_unidades(str_nombre,str_direccion,num_numero_aptos) values 
-		('$_REQUEST[nombre]','$_REQUEST[direccion]',$_REQUEST[noapartamentos])";
+	//query a ejecutar
+	$sql = "insert into tbl_unidades(str_nombre, str_direccion, num_numero_aptos, dtm_fecha_crea) values 
+		('$_REQUEST[nombre]', '$_REQUEST[direccion]', $_REQUEST[noapartamentos], now())";
 
-	echo $sql;
-	
-
+	//echo $sql."<br>";	
+	//ejecuto el insert de la unidad
 	if(!$result = $db->query($sql)){
 	    die('There was an error running the query [' . $db->error . ']');
 	}
 	else
 	{
-		echo "La unidad fue creada.";
+		echo "La unidad fue creada."."<br>";
+
 	}
-	$mysqli->close();
+
+	//Consulta de las unidades
+	$query = "SELECT * FROM tbl_unidades ORDER by dtm_fecha_crea";
+	$result = $db->query($query);
+
+	while($row = $result->fetch_array(MYSQLI_ASSOC))
+	{
+		$rows[] = $row;
+	}
+
+	foreach($rows as $row)
+	{
+		printf ("Nombre: %s, Direccion: %s, Numero Aptos: %d, Fecha Creacion: %s\n <br>", 
+		$row["Str_Nombre"], $row["Str_Direccion"], $row["Num_Numero_aptos"], $row["Dtm_Fecha_Crea"]);
+	}
+
+	/* free result set */
+	$result->close();
+
+	/* close connection */
+	$db->close();
 	
 	?>
 </body>
