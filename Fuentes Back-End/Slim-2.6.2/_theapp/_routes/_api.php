ede7300707a1c4132eda9bf004593edd0911b996
Orlando Tamayo Llanos <ortalla_9@hotmail.com>
2015-09-16 19:49:33 -0500

    Back

---
+++ Fuentes Back-End/Slim-2.6.2/_theapp/_routes/_api.php
@@ -0,0 +1,6 @@
+<?php if(!defined('CONSGBL')) die ('No tiene permisos sobre este directorio'); //Bloque que confirma que se debe haber pasnado primero por el enrrutador 
+
+/*SERVICIO PARA xxxxxxxxxxxxxxx */
+$theApp->get('/', function() use($theApp){
+	
+});
