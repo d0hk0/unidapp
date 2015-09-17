5b5feb65182e558eb1df6a7e1fe1fb1c26b3c115
d0hk0 <darkurt@gmail.com>
2015-09-16 20:52:33 -0500

    /anuncios_gen/
    apirest anuncios generales

--- Fuentes Back-End/Slim-2.6.2/_theapp/_routes/_api.php
+++ Fuentes Back-End/Slim-2.6.2/_theapp/_routes/_api.php
@@ -5,7 +5,7 @@ $theApp->get('/', function() use($theApp){
 	
 });
 
-/*SERVICIO PARA CREAR UN NUEVO PRODUCTO*/
+/*SERVICIO PARA CREAR UN NUEVO ANUNCIO ADMINISTRADOR*/
 $theApp->post('/anuncios_admin/', function() use($theApp){
 
 	$request = $theApp->request();
@@ -21,6 +21,42 @@ $theApp->post('/anuncios_admin/', function() use($theApp){
 		$getConnection = connect();
 
 		$query = $getConnection->prepare('INSERT INTO tbl_anuncios_admin VALUES (null,?,?,?,?,now())');
+		$query->bindParam(1, $titulo);
+		$query->bindParam(2, $descripcion);
+		$query->bindParam(3, $criterio);
+		$query->bindParam(4, $usuarioid);
+		$query->execute();
+		$prdId = $getConnection->lastInsertId();
+		$getConnection =  null;
+
+		$theApp->response->headers->set('Content-type', 'application/json');
+		$theApp->response->status(200);
+
+		$datosRes = array('message'=>$prdId);
+		$theApp->response->body(json_encode($datosRes));
+
+	} catch (PDOException $e) {
+		echo 'Error -> ' . $e->getMessage();
+	}
+});
+
+
+/*SERVICIO PARA CREAR UN NUEVO ANUNCIO ADMINISTRADOR*/
+$theApp->post('/anuncios_gen/', function() use($theApp){
+
+	$request = $theApp->request();
+	   $body = $request->getBody();
+	   $input = json_decode($body); 
+
+	$titulo	 = (string)$input->titulo_anungen;
+	$descripcion = (string)$input->descripcion_anungen;
+	$criterio = (string)$input->criterio_anungen;
+	$usuarioid = (string)$input->usuarioid_anungen;
+	
+	try {
+		$getConnection = connect();
+
+		$query = $getConnection->prepare('INSERT INTO tbl_anuncios_gen VALUES (null,?,?,now(),?,?)');
 		$query->bindParam(1, $titulo);
 		$query->bindParam(2, $descripcion);
 		$query->bindParam(3, $criterio);
