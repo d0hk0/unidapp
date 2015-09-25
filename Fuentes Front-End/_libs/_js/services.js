62c25ac7a840e1ef15e96591d6ad2af5b996cd76
tama8947 <ortalla_9@hotmail.com>
2015-09-25 08:49:48 -0500

    A

--- Fuentes Front-End/_libs/_js/services.js
+++
@@ -1,9 +0,0 @@
-angular.module("mainApp")
-.factory("xxxxxxResource", function($resource) {
-	return $resource(
-			"http://localhost/xxxxxxxxxxxxxxxxxxxx/:id_mov", 
-			//"http://www.xxxxxxxxxxxxxxxxxxxxxx/:id_mov",
-			{id_mov: "@id_mov"},
-			{update: {method: "PUT"}}
-			);
-})
