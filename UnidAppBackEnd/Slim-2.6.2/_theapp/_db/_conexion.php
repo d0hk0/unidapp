1d5705f5245a5ea4b15948b5340ecff04d138ba1
d0hk0 <darkurt@gmail.com>
2016-03-24 15:18:09 -0500

    cambio conexion bd a jelastic

--- UnidAppBackEnd/Slim-2.6.2/_theapp/_db/_conexion.php
+++ UnidAppBackEnd/Slim-2.6.2/_theapp/_db/_conexion.php
@@ -5,10 +5,11 @@ if(!defined('CONSGBL')) die ('No tiene permisos sobre este directorio'); //Bloqu
 function connect(){
 	try {
 		//Se definen los parametros del usuario y contraseña de la Base de Datos
-		$user_db = 'root'; $pass_db = '';//local
+		//$user_db = 'root'; $pass_db = '';//local
+		$user_db = 'root'; $pass_db = '5gDE8hKNQB';//jelastic
 		//$user_db = 'titandes_apis'; $pass_db = 'user.apis';//server
 		
-		$connection = new PDO('mysql:host=localhost;dbname=bdunidapp', $user_db, $pass_db);//local
+		$connection = new PDO('mysql:host=https://mysql8883-env-comunidapp.dal.jelastic.vps-host.net;dbname=bdunidapp', $user_db, $pass_db);//local
 		//$connection = new PDO('mysql:host=localhost;dbname=titandes_apirestful01', $user_db, $pass_db);//server
 		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
