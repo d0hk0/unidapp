749e850c1db6c0749d40ea38a4a861abec38aaf7
d0hk0 <darkurt@gmail.com>
2016-03-24 17:53:16 -0500

    prueba conexion

--- testmysql.php
+++ testmysql.php
@@ -1,9 +1,13 @@
 <html>
 <?php 
-$link = mysql_connect('https://node8894-unidapp-ds.dal.jelastic.vps-host.net','root','qdZvnAKE9o'); 
-if (!$link) { 
-	die('Could not connect to MySQL: ' . mysql_error()); 
-} 
-echo 'Connection OK'; mysql_close($link); 
+	/*$link = mysql_connect('https://node8894-unidapp-ds.dal.jelastic.vps-host.net','root','qdZvnAKE9o'); 
+	if (!$link) { 
+		die('Could not connect to MySQL: ' . mysql_error()); 
+	} */
+
+	$link = mysql_connect('node8894-unidapp-ds.dal.jelastic.vps-host.net', 'root', 'qdZvnAKE9o');
+	$selected = mysql_select_db('bdunidapp',$link);
+
+	echo 'Connection OK'; mysql_close($link); 
 ?> 
 </html>
