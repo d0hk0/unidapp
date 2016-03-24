8fc49cfe730180bb734d66d395567ac1b16872a2
d0hk0 <darkurt@gmail.com>
2016-03-24 18:06:13 -0500

    rtr

--- testmysql.php
+++ testmysql.php
@@ -1,13 +1,15 @@
 <html>
+<body>
 <?php 
 	/*$link = mysql_connect('https://node8894-unidapp-ds.dal.jelastic.vps-host.net','root','qdZvnAKE9o'); 
 	if (!$link) { 
 		die('Could not connect to MySQL: ' . mysql_error()); 
 	} */
-
+	echo "algo";
 	$link = mysql_connect('node8894-unidapp-ds.dal.jelastic.vps-host.net', 'root', 'qdZvnAKE9o');
 	$selected = mysql_select_db('bdunidapp',$link);
 
 	echo 'Connection OK'; mysql_close($link); 
 ?> 
+</body>
 </html>
