85e2412ffd1c436ab509e7381853674aeae1dd1f
d0hk0 <darkurt@gmail.com>
2016-03-24 21:25:12 -0500

    gfg

--- UnidAppFrontEnd/_libs/_js/services.js
+++ UnidAppFrontEnd/_libs/_js/services.js
@@ -8,13 +8,13 @@ angular.module("unidApp")
 })
 .factory("s_anuncios_g", function($resource){
 	//return $resource("http://localhost/GitHub/unidapp/UnidAppBackEnd/Slim-2.6.2/anunciosgen/", {update: {method: "PUT"}});
-	return $resource("https://unidapp-ds.dal.jelastic.vps-host.net/ROOT/UnidAppBackEnd/Slim-2.6.2/anunciosgen/", {update: {method: "PUT"}});
+	return $resource("http://unidapp-ds.dal.jelastic.vps-host.net/ROOT/UnidAppBackEnd/Slim-2.6.2/anunciosgen/", {update: {method: "PUT"}});
 })
 .factory("s_criterios", function($resource){
 	//return $resource("http://localhost/GitHub/unidapp/UnidAppBackEnd/Slim-2.6.2/criterio/", {update: {method: "PUT"}});
-	return $resource("https://unidapp-ds.dal.jelastic.vps-host.net/ROOT/UnidAppBackEnd/Slim-2.6.2/criterio/", {update: {method: "PUT"}});
+	return $resource("http://unidapp-ds.dal.jelastic.vps-host.net/ROOT/UnidAppBackEnd/Slim-2.6.2/criterio/", {update: {method: "PUT"}});
 })
 .factory("s_anuncios_a", function($resource){
 	//return $resource("http://localhost/GitHub/unidapp/UnidAppBackEnd/Slim-2.6.2/anunciosadmin/", {update: {method: "PUT"}});
-	return $resource("https://unidapp-ds.dal.jelastic.vps-host.net/ROOT/UnidAppBackEnd/Slim-2.6.2/anunciosadmin/", {update: {method: "PUT"}});
+	return $resource("http://unidapp-ds.dal.jelastic.vps-host.net/ROOT/UnidAppBackEnd/Slim-2.6.2/anunciosadmin/", {update: {method: "PUT"}});
 })
